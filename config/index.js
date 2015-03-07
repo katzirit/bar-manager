ce36dd07f8942b2e72c873756963d9ec065bf690
Daniel Cohen <dcohenb@gmail.com>
2015-03-07 05:25:59 +0200

    a

--- config/index.js
+++ config/index.js
@@ -1,15 +1,7 @@
-/*module.exports = {
-    username: "pingme_at",
-    password: "]4B+Rdw5}jW(}*u",
-    database: "itai",
-    host: "pingme.at",
-    dialect: "mysql"
-};*/
-
 module.exports = {
-    username: "katzirit",
-    password: "29361",
-    database: "katzirit",
-    host: "my.jce.ac.il",
+    username: "root",
+    password: "",
+    database: "barmanager",
+    host: "localhost",
     dialect: "mysql"
 };
