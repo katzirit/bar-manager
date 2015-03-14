f1a292f2619e05380334de631ad0e822e43164ae
Itai Katzir <katzir66@gmail.com>
2015-03-14 20:12:45 +0200

    changes with daniel 14/3/2015
    repair user access to simulation aute.
    change date to short format.
    error ,assage display at view in add simulation and login.

--- public/js/controllers/views/SimulationsAddCtrl.js
+++ public/js/controllers/views/SimulationsAddCtrl.js
@@ -1,7 +1,7 @@
 
 app.controller('SimulationsAddCtrl', function ($scope, $http, $state) {
     // Simulation default values
-    $scope.data = {
+    $scope.simulation = {
         data: {
             costumers_per_hour: 100,
             num_of_waiters: 5,
@@ -17,12 +17,12 @@ app.controller('SimulationsAddCtrl', function ($scope, $http, $state) {
      *
      */
     $scope.submitHandler = function () {
-        $http.put('/api/simulations', $scope.data)
+        $http.put('/api/simulations', $scope.simulation)
             .success(function (simulation) {
                 $state.go('simulation', {id: simulation.id});
             })
-            .error(function (data) {
-                console.error(data.info);
+            .error(function (response) {
+                $scope.error = response.info;
             });
     };
 });
