701fe05f89e4256266c1bda9a05bf68296a6146b
Itai Katzir <katzir66@gmail.com>
2015-03-25 11:11:06 +0200

    egfsdg
    dfgs

--- modules/simulator.js
+++ modules/simulator.js
@@ -2,9 +2,9 @@ var _ = require('lodash');
 var math = require('mathjs');
 var avg_line = 1;
 var avg_service = 1;
-var beer = 1;
-var soft_drinks = 1;
-var booz = 1;
+var beer = 0;
+var soft_drinks = 0;
+var booz = 0;
 var profit = 1;
 var employee_efficiency = 1;
 var events = [];
@@ -27,13 +27,24 @@ var tSit;
 var costomer_arrive;
 var avg_service_time;
 var service_div;
+var u;
+var x;
+var u1;
+var u2;
+var x;
+var myNorm;
+var timediv;
+var timediv1;
+var timediv2;
+var service;
+var disc = Math.random();
+var isStay = Math.random();
 
 function costomerArriveCreate(){
-     var u;
-     var x;
+
      u = Math.random();
      x = (-1 / costomer_arrive)*Math.log(1-u);
-     var timediv1 = x + tnow;
+     timediv1 = x + tnow;
      events.push({
             time: timediv1,
             code: 1
@@ -42,32 +53,23 @@ function costomerArriveCreate(){
 }
 
 function serviceEndCreate(){
-    var u1 = Math.random();
-    var u2 = Math.random();
-    var x = Math.sqrt(-2 * Math.log(u1)) * Math.Cos((2 * Math.PI()) * u2);
-    var myNorm = avg_service_time + Math.sqrt(service_div) * x;
-    var timediv = tnow + myNorm;
+         u1 = Math.random();
+         u2 = Math.random();
+         x = Math.sqrt(-2 * Math.log(u1)) * Math.Cos((2 * 3.1415) * u2);
+         myNorm = service + Math.sqrt(service_div) * x;
+         timediv = tnow + (service + Math.sqrt(service_div) * x);
      events.push({
             time: timediv,
             code: 2
         });
 }
 
-<<<<<<< HEAD
 function clearTableCreate(){
-     var timediv2 = tnow + 0.1;
+     timediv2 = tnow + 0.1;
      events.push({
             time:timediv2,
             code: 3
         });
-=======
-function clearTableCreate() {
-    timediv2 = tnow + 0.1;
-    events.push({
-        time: timediv2,
-        code: 3
-    });
->>>>>>> parent of e589783... simulation
 }
 
 
@@ -80,7 +82,7 @@ function costomerArrive(){
             server--;
             serviceEndCreate();
         } else {
-            wait_avg = wait_avg+(tnow - wait_last)*wait;
+            wait_avg = wait_avg+(tnow - wait_last) * wait;
             wait_last = tnow;
             wait++;
         }
@@ -95,8 +97,6 @@ function costomerArrive(){
     }
 
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
 function ezer(){
@@ -107,26 +107,12 @@ function ezer(){
 
 function serviceEnd(){
      wait--;
-     var disc = Math.random();
-     var isStay = Math.random();
      if (disc() < 0.4){
-=======
-
-=======
-
->>>>>>> parent of e589783... simulation
-function serviceEnd() {
-    wait--;
-    var disc = Math.random();
-    var isStay = Math.random();
-    if (disc < 0.4) {
->>>>>>> parent of e589783... simulation
         beer++;
      } else if (disc < 0.8){
         booz++;
      } else {
         soft_drinks++;
-<<<<<<< HEAD
      }
      if (isStay <0.7){
         if (wait==0){
@@ -135,41 +121,17 @@ function serviceEnd() {
             serviceEndCreate();
         }
      } else {
-=======
-    }
-    if (isStay < 0.7) {
-        if (wait == 0) {
-            server++;
-            server_avg = server_avg + (tnow - server_last) * server;
-            server_last = tnow;
-            
-        } else {
-            serviceEndCreate();
-        }
-    } else {
-<<<<<<< HEAD
->>>>>>> parent of e589783... simulation
-=======
->>>>>>> parent of e589783... simulation
         clearTableCreate();
      }
 
 }
 
 
-<<<<<<< HEAD
 function clearTable(){
      table_num--;
      sit_avg = server_avg + (tnow - tSit);
      tSit = tnow;
      if (l>0){
-=======
-function clearTable() {
-    table_num--;
-    sit_avg = sit_avg + (tnow - tSit);
-    tSit = tnow;
-    if (l > 0) {
->>>>>>> parent of e589783... simulation
         l--;
         l_change++;
      }
@@ -186,22 +148,12 @@ module.exports = function (data, numOfRuns) {
      data.avg_service_time
      data.avg_service_time
     */
-<<<<<<< HEAD
-<<<<<<< HEAD
-    avg_service_time = data.avg_service_time;
+    avg_service_time = 0;
+    service = data.avg_service_time;
     service_div = data.avg_service_div;
     server = data.num_of_waiters;
-    tmax = 8;
+    tmax = data.shift_length*60;
     tnow=0.0;
-=======
-=======
->>>>>>> parent of e589783... simulation
-    avg_service_time = 0.25;
-    service_div = 0.1;
-    server = 1;
-    tmax = 8;
-    tnow = 0.0;
->>>>>>> parent of e589783... simulation
     l = 0;
     l_in = 0.0;
     l_change = 0;
@@ -209,8 +161,8 @@ module.exports = function (data, numOfRuns) {
     server_last = 0;
     sit_avg = 0;
     tSit = 0;
-    table_max = 20;
-    costomer_arrive = 5;
+    table_max = data.table_num;
+    costomer_arrive = data.costumers_per_hour;
     var results = [];
     var i =0;
      events.push({
@@ -220,7 +172,6 @@ module.exports = function (data, numOfRuns) {
 
 
 
-<<<<<<< HEAD
     while ((tmax>tnow)){
             tnow = events[i].time;
             switch (events[i].code) {
@@ -237,46 +188,18 @@ module.exports = function (data, numOfRuns) {
             i++;       
 
      }
-
+    employee_efficiency =(server_avg / (tmax * server))*100;
+    server_avg = server_avg / server;
     // Dummy simulator
     _.times(numOfRuns, function () {
         results.push({
-            avg_line: server,
+            avg_line: tnow + (service + Math.sqrt(service_div) * x),
             avg_service: avg_service,
-=======
-    while ((tmax > tnow)) {
-        tnow = events[i].time;
-        switch (events[i].code) {
-            case 1:
-                costomerArrive();
-                break;
-            case 2:
-                serviceEnd();
-                break;
-            case 3:
-                clearTable();
-                break;
-        }
-        i++;
-
-    }
-    avg_line = l_in / l_change;
-    profit = ((beer * beerPrice) + (soft_drinks * softPrice) + (booz * boozPrice)) - server*minWage;
-    employee_efficiency = server_avg / (tmax * server);
-    // Dummy simulator
-    _.times(numOfRuns, function() {
-        results.push({
-            avg_line: l_change,
-            avg_service: events[1].code,
-<<<<<<< HEAD
->>>>>>> parent of e589783... simulation
-=======
->>>>>>> parent of e589783... simulation
             beer: beer,
             soft_drinks: soft_drinks,
             booz: booz,
-            profit: profit,
-            employee_efficiency: server_avg
+            profit: u1,
+            employee_efficiency: tnow
         });
     });
 
