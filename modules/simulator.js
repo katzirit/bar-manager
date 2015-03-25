e5897834c6426723cd976196da68990615cb773d
Itai Katzir <katzir66@gmail.com>
2015-03-25 09:07:18 +0200

    simulation

--- modules/simulator.js
+++ modules/simulator.js
@@ -63,7 +63,7 @@ function serviceEndCreate() {
 }
 
 function clearTableCreate() {
-    timediv2 = tnow + 0.1;
+    timediv2 = tnow + 0.05;
     events.push({
         time: timediv2,
         code: 3
@@ -97,8 +97,17 @@ function costomerArrive() {
     }
 
 }
+function avgser() {
+    if ( (tnow - server_last)>0 ){
+        server_avg = server_avg + ( tnow - server_last ) * server;
+         server_last = tnow;
+       server++;
+   }
+
+ }
 
 function serviceEnd() {
+
     wait--;
     var disc = Math.random();
     var isStay = Math.random();
@@ -111,14 +120,12 @@ function serviceEnd() {
     }
     if (isStay < 0.7) {
         if (wait == 0) {
-            server++;
-            server_avg = server_avg + (tnow - server_last) * server;
-            server_last = tnow;
-            
+                    avgser();        
         } else {
             serviceEndCreate();
         }
     } else {
+        avgser();
         clearTableCreate();
     }
 
@@ -126,6 +133,7 @@ function serviceEnd() {
 
 
 function clearTable() {
+       
     table_num--;
     sit_avg = sit_avg + (tnow - tSit);
     tSit = tnow;
@@ -146,10 +154,10 @@ module.exports = function(data, numOfRuns) {
      data.avg_service_time
      data.avg_service_time
     */
-    avg_service_time = 0.25;
-    service_div = 0.1;
-    server = 1;
-    tmax = 8;
+    avg_service_time = data.avg_service_time;
+    service_div = data.avg_service_div;
+    server = data.num_of_waiters;
+    tmax = data.shift_length *60;
     tnow = 0.0;
     l = 0;
     l_in = 0.0;
@@ -158,8 +166,8 @@ module.exports = function(data, numOfRuns) {
     server_last = 0;
     sit_avg = 0;
     tSit = 0;
-    table_max = 20;
-    costomer_arrive = 5;
+    table_max = data.table_num;
+    costomer_arrive = data.costumers_per_hour;
     var results = [];
     var i = 0;
     events.push({
@@ -177,6 +185,7 @@ module.exports = function(data, numOfRuns) {
                 break;
             case 2:
                 serviceEnd();
+
                 break;
             case 3:
                 clearTable();
@@ -187,19 +196,19 @@ module.exports = function(data, numOfRuns) {
     }
     avg_line = l_in / l_change;
     profit = ((beer * beerPrice) + (soft_drinks * softPrice) + (booz * boozPrice)) - server*minWage;
-    employee_efficiency = server_avg / (tmax * server);
+    employee_efficiency =(server_avg / (tmax * server))*100;
+    server_avg = server_avg / server;
     // Dummy simulator
-    _.times(numOfRuns, function() {
         results.push({
-            avg_line: l_change,
-            avg_service: events[1].code,
+            avg_line: data.num_of_waiters,
+            avg_service: server_avg,
             beer: beer,
             soft_drinks: soft_drinks,
             booz: booz,
             profit: profit,
-            employee_efficiency: server_avg
+            employee_efficiency: employee_efficiency
         });
-    });
+
 
     return results;
 };
